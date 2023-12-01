f7b4ecbe32656776cc388a562f8944719ba99f56
nobleator <bn2471f9d@gmail.com>
2023-12-01 08:40:31 -0500

    Add geocoding rate limit hack

--- app/Geocoder.fs
+++ app/Geocoder.fs
@@ -1,4 +1,5 @@
 module Geocoder
+    open System.Threading.Tasks
     let [<Literal>] baseUri = "https://geocode.maps.co/search?q="
     let [<Literal>] sample = baseUri + "1600+Pennsylvania+Avenue,+Washington,+DC"
     type GeocodedLocation = FSharp.Data.JsonProvider<sample>
@@ -6,6 +7,8 @@ module Geocoder
     let geocode s =
         let uri = baseUri+System.Web.HttpUtility.UrlEncode(str=s)
         let res = GeocodedLocation.Load(uri) |> Array.toList
+        // TODO: clean this up, just adding a hack to avoid getting IP blocked
+        Task.Delay(1000).Wait()
         match res with
             | [] -> None
             | h::_ -> Some({
